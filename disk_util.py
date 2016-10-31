b7ce37d6892f71480290168a263e95fce680e1a1
Elbert Fliek <elbert@crowdynews.com>
2016-10-31 12:46:28 +0100

    removed unneed functions

--- disk_util.py
+++ disk_util.py
@@ -1,26 +1,4 @@
-import os
 import mmap
-import json
-import errno
-
-
-def make_sure_path_exists(path):
-	try:
-		os.makedirs(path)
-	except OSError as exception:
-		if exception.errno != errno.EEXIST:
-			raise
-
-
-def save_json(data, filename):
-	with open(filename + '.json', 'wb') as f:
-		json.dump(data, f, indent=2)
-
-
-def unsure_folder_name(path):
-	if path[-1] == '/':
-		return
-	return path + '/'
 
 
 def countlines(filename):
